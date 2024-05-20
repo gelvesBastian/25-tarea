0e3c09df2c5716a83ef6ea5ea03263537fb2e303
gelvesBastian <166016824+gelvesBastian@users.noreply.github.com>
2024-05-20 08:10:10 -0300

    tarea25
    terminarela

---
+++ archivo.js
@@ -0,0 +1,7 @@
+let usuario="juangarcia23"
+let contraseña="cjzoht3"
+if ((contraseña == "cjzoht3") && (usuario == "juangarcia23")) {
+    alert("bienvenido juansito")
+} else {
+  alert("usuario o contraseña incorrecto")  
+}
